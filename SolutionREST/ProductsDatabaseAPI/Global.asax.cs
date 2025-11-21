2d5fc836f2d35a7445618f1dc562d9ad137b2ca6
BERNARDO INÁCIO <2222402@my.ipleiria.pt>
2025-11-21 16:13:34 +0000

    nome api alterado

--- SolutionREST/ProductsDatabaseAPI/Global.asax.cs
+++ SolutionREST/ProductsDatabaseAPI/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
 
-namespace ProductsDatabaseAPI
+namespace MiddleWare
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
