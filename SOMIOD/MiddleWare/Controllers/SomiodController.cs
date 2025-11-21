f148e475ed8139c9abd32b606e3f8c79fa327c08
Francisco Crespo <2221482@my.ipleiria.pt>
2025-11-21 19:25:08 +0000

    Renomear pastas fisicas para coincidir com o Visual Studio

---
+++ SOMIOD/MiddleWare/Controllers/SomiodController.cs
@@ -0,0 +1,159 @@
+﻿using Newtonsoft.Json.Linq;
+using MiddleWare.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MiddleWare.Controllers
+{
+    
+    public class SomiodController : ApiController
+    {
+        string connectionString = Properties.Settings.Default.ConnStr;
+
+        // GET: api/Products
+        /// <summary>
+        /// Gets all products from database
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Product> Get()
+        {
+            List<Product> lista = new List<Product>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Prods", conn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Product prod = new Product();
+                        prod.Id = (int)reader["Id"];
+                        prod.Name = (string)reader["Name"];
+                        prod.Category = (string)reader["Category"];
+                        prod.Price = (decimal)reader["Price"];
+                        lista.Add(prod);
+                    }
+                }
+            }
+            return lista;
+        }
+
+        // GET: api/Products/5
+        public IHttpActionResult Get(int id)
+        {
+            Product prod = null;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Prods WHERE Id = @idProd", conn);
+                command.Parameters.AddWithValue("@idProd", id);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        prod = new Product();
+                        prod.Id = (int)reader["Id"];
+                        prod.Name = (string)reader["Name"];
+                        prod.Category = (string)reader["Category"];
+                        prod.Price = (decimal)reader["Price"];
+                    }
+                }
+            }
+            if (prod == null)
+            {
+                return NotFound();
+            }
+            else
+                return Ok(prod);
+        }
+
+        // POST: api/Products
+        public IHttpActionResult Post([FromBody] Product value)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "INSERT INTO Prods VALUES (@name, @cat, @price)";
+                    SqlCommand command = new SqlCommand(sql, conn);
+                    command.Parameters.AddWithValue("@name", value.Name);
+                    command.Parameters.AddWithValue("@cat", value.Category);
+                    command.Parameters.AddWithValue("@price", value.Price);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                        return Ok();
+                    else
+                        return BadRequest();
+
+                }
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+        // PUT: api/Products/5
+        public IHttpActionResult Put(int id, [FromBody]Product value)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "UPDATE Prods SET Name=@name, Category=@cat, Price=@price WHERE Id = @id";
+                    SqlCommand command = new SqlCommand(sql, conn);
+                    command.Parameters.AddWithValue("@name", value.Name);
+                    command.Parameters.AddWithValue("@cat", value.Category);
+                    command.Parameters.AddWithValue("@price", value.Price);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                        return Ok();
+                    else
+                        return BadRequest();
+                }
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+        // DELETE: api/Products/5
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "DELETE Prods WHERE Id = @id";
+                    SqlCommand command = new SqlCommand(sql, conn);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                        return Ok();
+                    else
+                        return BadRequest();
+                }
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
---
+++ SOMIOD/MiddleWare/Models/Content-Instance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace MiddleWare.Models
+{
+    [XmlRoot(ElementName = "content-instance")]
+    public class ContentInstance
+    {
+        [XmlIgnore] // Só é usado na BD
+        public int Id { get; set; }
+
+        
+        [XmlElement(ElementName = "resource-name")]
+        public string Name { get; set; }
+
+        
+        [XmlElement(ElementName = "creation-datetime")]
+        public string CreationDate { get; set; }
+
+        
+        [XmlElement(ElementName = "res-type")]
+        public string ResType { get; set; } = "content-instance";
+
+        
+        [XmlElement(ElementName = "content")]
+        public string Content { get; set; }
+
+        
+        [XmlElement(ElementName = "content-type")]
+        public string ContentType { get; set; }
+
+        
+        [XmlIgnore] // Foreign Key para ligar ao Container Pai
+        public int ParentId { get; set; }
+
+        public ContentInstance()
+        {
+            CreationDate = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+        }
+
+        public ContentInstance(int id, string name, string content, string contentType, int parentId)
+        {
+            Id = id;
+            Name = name;
+            Content = content;
+            ContentType = contentType;
+            ParentId = parentId;
+            CreationDate = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+        }
+    }
+}
